c8d61778d0cb66d15dbe23ac812dd9d613839f0e
Anjali <anjalikumari972001@gmail.com>
2022-08-29 00:26:59 +0530

    new changes

---
+++ functions.js
@@ -0,0 +1,18 @@
+console.log("Hi,My name is Anjali Kumari")
+
+
+let name1="Anjali";
+/*console.log(name+ " is a good girl.")// to add a name
+
+console.log(name1 + "is in gla.");*/
+let name2="Geeta";
+let greetText="Good Morning!"
+function greet(name,greetText)
+{
+    console.log(name + " is a good girl.");
+    console.log(greetText + " " +name)
+}
+greet(name1,greetText);
+greet(name2,greetText);
+// this function is made to make our work easier and simpler.
+// this is done to increase the code reuseability.
