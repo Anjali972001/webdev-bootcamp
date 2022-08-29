56d45779d6ef6ba73939918882d450efc0428320
Anjali <anjalikumari972001@gmail.com>
2022-08-29 21:16:01 +0530

    new   ..

--- functions.js
+++ functions.js
@@ -16,3 +16,11 @@ greet(name1,greetText);
 greet(name2,greetText);
 // this function is made to make our work easier and simpler.
 // this is done to increase the code reuseability.
+function sum(a,b,c)
+{
+    let d=a+b+c;
+    return d;
+
+}
+let returnValue=sum(10,20,30);
+console.log("sum of the given numbers is "+returnValue)
