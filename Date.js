1d90acb6c9df9d0dbd8bfbf9cc2ec06f631f881d
Anjali <anjalikumari972001@gmail.com>
2022-09-05 19:27:54 +0530

    new

---
+++ Date.js
@@ -0,0 +1,11 @@
+let myDate=new Date(); // new  keyword is used create the new date.
+console.log(myDate);
+console.log(myDate.getTime());// give the times in second
+console.log(myDate.getFullYear()); // the current year
+console.log(myDate.getDay());// the which is the no.of day is today
+// like 1=Monday,2=Tuesday
+
+console.log(myDate.getMinutes());// to print the minutes
+console.log(myDate.getHours());// to print the no of hours
+console.log(myDate.getDate());
+
