dc99203b32b9bbb4773994fc99152efac9b718b5
Anjali <anjalikumari972001@gmail.com>
2022-09-12 00:00:36 +0530

    new

--- bmiCalculator.js
+++ bmiCalculator.js
@@ -1,7 +1,7 @@
 function bmiCalculator(weight , height)
 {
-    var bmi=weight/Math.pow(height,2);
-    return Math.round(bmi) ;// math.
+    var bmi=weight/Math.pow(height,2);// math.pow()---> is used to get the power .
+    return Math.round(bmi) ;// math.round ---is used to round the decimal number.
 }
 var bmi=bmiCalculator(10,2);
 console.log(bmi);
